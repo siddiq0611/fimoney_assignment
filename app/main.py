76d17f55968f6a1c39548a840e2c2c9825657022
siddiq0611 <riyaz.2201165me@iiitbh.ac.in>
2025-07-25 23:36:53 +0530

    ...

--- app/main.py
+++ app/main.py
@@ -23,3 +23,22 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+    user = users_collection.find_one({"username": username})
+    if user is None:
+        raise credentials_exception
+    return user
